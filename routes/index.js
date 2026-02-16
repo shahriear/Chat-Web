f1600fd6edc99a0b80dbd0a9823527fdad80fd5f
Md. Shahriear Shuvo <shahriearshuvo@gmail.com>
2026-02-16 18:31:39 +0600

    server

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const apiRoute = require('./api');
 
 router.use('/api/v1', apiRoute);
+router.get("/",(req,res)=>{
+  res.send("Hello! Welcome to the Server.")
+})
 router.use((req, res) => {
   res.status(404).send('Page Not Found!');
 });
